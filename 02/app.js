1e8ac59b5b2587c02d5027b817bb9c362444d4e4
Ireneusz Cislo <irekc86@o2.pl>
2023-03-25 06:20:13 +0100

    task 02 finished

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,19 @@
 
 /* rozwiązanie z pętlą for */
+console.log('rozwiązanie z pętlą for');
 const x = 4;
 
+for(let i=1; i<=9; i++ ) {
+    const result = x * i;
+    console.log(`${x} x ${i} = ${result}`)
+}
 
 
 /* rozwiązanie z pętlą while  */
+console.log('rozwiązanie z pętlą while');
+let y = 1;
+
+while(y <= 10) {
+    console.log(`${x} x ${y} = ${x * y}`);
+    y++;
+}
