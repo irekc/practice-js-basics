577161d1c2a6e73ed787d2bc81326d1e06b9d2bf
Ireneusz Cislo <irekc86@o2.pl>
2023-03-25 06:48:50 +0100

    task 03 finished

--- 03/app.js
+++ 03/app.js
@@ -1,16 +1,52 @@
 const min = 1;
 const max = 100;
 
+const isEven = function(num) {
+    if(typeof num === 'number') {
+        return num % 2 === 0 ? true : false;
+    }
+    return null
+}
+
 const a = randomNumber(min, max);
 const b = randomNumber(min, max);
 const c = randomNumber(min, max);
+const sum = getSum(a, b, c);
+const even = isEven(sum);
 
 console.log(a, b, c);
 
 
+showInfo(sum, even);
 
 
+function getSum(number1, number2, number3) {
+    if(number1>number3 && number2>number3) {
+        return number1 + number2
+    }
+    if(number1>number2 && number3>number2) {
+        return number1 + number3
+    }
+    if(number2>number1 && number3>number1) {
+        return number3 + number2
+    }
+}
 
+function showInfo(num, arg) {
+    let info
+    switch(arg) {
+        case null:
+            info = `podany argument ${num} nie jest liczbą`;
+            break;
+        case true:
+            info =  `podany argument ${num} jest parzysty`;
+            break;
+        case false:
+            info =  `podany argument ${num} jest nieparzysty`;
+            break;
+    }
+    return console.log(info)
+}
 
 
 function randomNumber(min, max) {
