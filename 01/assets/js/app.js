71d5f57a838df48a885095a24a3b54c583898fb7
Ireneusz Cislo <irekc86@o2.pl>
2023-03-25 06:08:11 +0100

    task 01 finished

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -2,3 +2,19 @@ const a = '4.2';
 const b = 9;
 
 console.log(a, b);
+console.log(typeof a, typeof b)
+
+const sum = parseFloat(a) + parseFloat(b);
+const substr = a - b;
+const multipl = a * b;
+const divide = a/b;
+
+console.log(sum, substr, multipl, divide);
+
+const arr = [sum, substr, multipl, divide];
+arr.forEach(function(number) {
+    if(number > 20) {
+        return console.log(number + ' jest większy od 20')
+    }
+    return console.log(number + ' jest mniejszy lub równy 20')
+})
